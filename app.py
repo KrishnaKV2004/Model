18f764976c9bdc7b625684ef794b33af6a746f8c
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-02-22 12:20:20 +0530

    Flask Model

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html", page="home")
+
+@app.route('/model')
+def model():
+    return render_template("home.html", page="model")
+
+@app.route('/about')
+def about():
+    return render_template("home.html", page="about")
+
+@app.route('/help')
+def help():
+    return render_template("home.html", page=help)
 
 if __name__ == "__main__":
     app.run(debug=True)
